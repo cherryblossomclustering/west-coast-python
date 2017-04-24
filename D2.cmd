6f4464a8817db52c4b7680c34a4e2b0d515747cc
travisnguyen <travis.giuse.nguyen@gmail.com>
2017-04-24 00:54:24 -0700

    D2

--- D2.cmd
+++ D2.cmd
@@ -4,7 +4,7 @@ getenv = true
 output = D2.out
 error = D2.error
 log = D2.log
-arguments = "./ROUGE-1.5.5.pl -e /dropbox/16-17/573/code/ROUGE/data -a -n 4 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d rouge_run.xml"
+arguments = "python3 centroid.py corpora.json 10 20 brown"
 transfer_executable = false
 request_memory = 1024*2
 queue
