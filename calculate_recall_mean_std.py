741879e2daacc549f8ad0bf99ec8f2c4f59221b5
Travis Nguyen <travin@uw.edu>
2017-05-11 15:51:40 -0700

    edited files in examples/ and variable names in calculated_recall_mean_std.py

--- calculate_recall_mean_std.py
+++ calculate_recall_mean_std.py
@@ -31,10 +31,10 @@ output_file = args.output_file
 
 logging.basicConfig(filename='calculate_recall_mean_std.log', level=logging.DEBUG)
 
-rouge1_recall = list()
-rouge2_recall = list()
-rouge3_recall = list()
-rouge4_recall = list()
+rouge1_r_list = list()
+rouge2_r_list = list()
+rouge3_r_list = list()
+rouge4_r_list = list()
 
 prog = re.compile("^([0-9A-Z]+) ROUGE-([1-4]?) Average_R: (\d*\.?\d*) \(95%-conf.int. (\d*\.?\d*) - (\d*\.?\d*)\)$")
 
@@ -42,7 +42,7 @@ with open(scoring_file, 'r+') as f:
     for line in f:
         result = prog.match(line)
 
-        if(result):
+        if(result is not None):
             doc_id = str(result.group(1))
             ngram_size = int(result.group(2))
             recall = float(result.group(3))
@@ -50,60 +50,60 @@ with open(scoring_file, 'r+') as f:
             ci_upper_bound = float(result.group(5))
 
             if(ngram_size == 1):
-                rouge1_recall.append(recall)
+                rouge1_r_list.append(recall)
             elif(ngram_size == 2):
-                rouge2_recall.append(recall)
+                rouge2_r_list.append(recall)
             elif(ngram_size == 3):
-                rouge3_recall.append(recall)
+                rouge3_r_list.append(recall)
             elif(ngram_size == 4):
-                rouge4_recall.append(recall)
+                rouge4_r_list.append(recall)
             else:
                 logging.warning("Invalid ROUGE n-gram size: %s." % (recall))
 
-rouge1_mean = numpy.mean(rouge1_recall)
-rouge2_mean = numpy.mean(rouge2_recall)
-rouge3_mean = numpy.mean(rouge3_recall)
-rouge4_mean = numpy.mean(rouge4_recall)
+rouge1_mean = numpy.mean(rouge1_r_list)
+rouge2_mean = numpy.mean(rouge2_r_list)
+rouge3_mean = numpy.mean(rouge3_r_list)
+rouge4_mean = numpy.mean(rouge4_r_list)
 
-rouge1_std = numpy.std(rouge1_recall)
-rouge2_std = numpy.std(rouge2_recall)
-rouge3_std = numpy.std(rouge3_recall)
-rouge4_std = numpy.std(rouge4_recall)
+rouge1_std = numpy.std(rouge1_r_list)
+rouge2_std = numpy.std(rouge2_r_list)
+rouge3_std = numpy.std(rouge3_r_list)
+rouge4_std = numpy.std(rouge4_r_list)
 
-rouge1_recall_min = min(rouge1_recall)
-rouge2_recall_min = min(rouge2_recall)
-rouge3_recall_min = min(rouge3_recall)
-rouge4_recall_min = min(rouge4_recall)
+rouge1_r_min = min(rouge1_r_list)
+rouge2_r_min = min(rouge2_r_list)
+rouge3_r_min = min(rouge3_r_list)
+rouge4_r_min = min(rouge4_r_list)
 
-rouge1_recall_max = max(rouge1_recall)
-rouge2_recall_max = max(rouge2_recall)
-rouge3_recall_max = max(rouge2_recall)
-rouge4_recall_max = max(rouge2_recall)
+rouge1_r_max = max(rouge1_r_list)
+rouge2_r_max = max(rouge2_r_list)
+rouge3_r_max = max(rouge2_r_list)
+rouge4_r_max = max(rouge2_r_list)
 
 with open(output_file, 'w+') as f:
     f.write("ROUGE-1" + '\n')
     f.write("Mean: %f" % (rouge1_mean) + '\n')
     f.write("Standard deviation: %f" % (rouge1_std) + '\n')
-    f.write("Minimum recall score: %f" % (rouge1_recall_min) + '\n')
-    f.write("Maximum recall score: %f" % (rouge1_recall_max) + '\n')
+    f.write("Minimum recall score: %f" % (rouge1_r_min) + '\n')
+    f.write("Maximum recall score: %f" % (rouge1_r_max) + '\n')
     f.write('\n')   
 
     f.write("ROUGE-2" + '\n')
     f.write("Mean: %f" % (rouge2_mean) + '\n')
     f.write("Standard deviation: %f" % (rouge2_std) + '\n')
-    f.write("Minimum recall score: %f" % (rouge2_recall_min) + '\n')
-    f.write("Maximum recall score: %f" % (rouge2_recall_max) + '\n')
+    f.write("Minimum recall score: %f" % (rouge2_r_min) + '\n')
+    f.write("Maximum recall score: %f" % (rouge2_r_max) + '\n')
     f.write('\n')
 
     f.write("ROUGE-3" + '\n')
     f.write("Mean: %f" % (rouge3_mean) + '\n')
     f.write("Standard deviation: %f" % (rouge3_std) + '\n')
-    f.write("Minimum recall score: %f" % (rouge3_recall_min) + '\n')
-    f.write("Maximum recall score: %f" % (rouge3_recall_max) + '\n')
+    f.write("Minimum recall score: %f" % (rouge3_r_min) + '\n')
+    f.write("Maximum recall score: %f" % (rouge3_r_max) + '\n')
     f.write('\n')
 
     f.write("ROUGE-4" + '\n')
     f.write("Mean: %f" % (rouge4_mean) + '\n')
     f.write("Standard deviation: %f" % (rouge4_std) + '\n')
-    f.write("Minimum recall score: %f" % (rouge4_recall_min) + '\n')
-    f.write("Maximum recall score: %f" % (rouge4_recall_max) + '\n')
+    f.write("Minimum recall score: %f" % (rouge4_r_min) + '\n')
+    f.write("Maximum recall score: %f" % (rouge4_r_max) + '\n')
