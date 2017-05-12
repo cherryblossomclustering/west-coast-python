bc3a4ef1200aa7fed2d89a1ed8efc5caf4a216cb
Travis Nguyen <travin@uw.edu>
2017-05-11 19:40:33 -0700

    updated generated_settings_xml.py and created new settings.xml example

--- generate_settings_xml.py
+++ generate_settings_xml.py
@@ -1,97 +1,89 @@
 #!/bin/usr/python
 
+import argparse
+import pprint
+from copy import deepcopy
 from os import listdir
 from os.path import isfile, join
-from collections import OrderedDict
-import pprint
 
-# hard-coded
-version='1.5.5'
-mypath = './outputs/D2'
-model_root = '/dropbox/16-17/573/Data/models/devtest'
-
-def merge_two_dicts(x, y):
-    """Given two dicts, merge them into a new dict as a shallow copy."""
-    z = x.copy()
-    z.update(y)
-    return z
-
-class Vividict(OrderedDict):
+class Vividict(dict):
     def __missing__(self, key):
         value = self[key] = type(self)()
         return value
 
-def recursive_print(dict1, dict2, depth, string):
-    for k, v in dict1.items():
-        if depth == 3:
+parser = argparse.ArgumentParser()
+parser.add_argument("version", action="store", nargs='?', default="1.5.5", type=str)
+parser.add_argument("model_root", action="store", type=str)
+parser.add_argument("system_root", action="store", type=str)
+parser.add_argument("output_file", action="store", type=str)
+args = parser.parse_args()
+version = args.version
+model_root = args.model_root
+system_root = args.system_root
+output_file = args.output_file
 
-            new_string = string + "." + k
-            
+def create_vividict(files):
+    vividict = Vividict()
 
-            print("<EVAL ID=\"%s\">" % (new_string))
-            print("<PEER-ROOT>")
-            print(mypath)
-            print("</PEER-ROOT>")
-            print("<MODEL-ROOT>")
-            print(model_root)
-            print("</MODEL-ROOT>")
-            # print(string)
-            
-            print("<INPUT-FORMAT TYPE=\"SPL\">")
-            print("</INPUT-FORMAT>")
-            print("<PEERS>")
-       
-            for x, y in v.items():
-                print("<P ID=\"%s\">%s</P>" % (x, ".".join([new_string, x])))
-                # print(x)
+    for f in files:
+        tokens = f.split('.')
 
-            print("</PEERS>")
-            print("<MODELS>")
-            
-            x = new_string.split(".")
-            for x, y in dict2[x[0]][x[1]][x[2]].items():
-                print("<M ID=\"%s\">%s</M>" % (x, ".".join([new_string, x])))
-                # print(x)
+        token1, token2, token3, token4, token5 = tokens
 
-            print("</MODELS>")
-            print("</EVAL>")
-        else:
-            if string == "":
-                new_string = string + k
-            else:
-                new_string = string + "." + k
-            recursive_print(v, dict2, depth+1, new_string)
+        vividict[token1][token2][token3][token4][token5]
 
-model_list = list()
+    return vividict
 
-files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-d = Vividict()
+def create_settings_xml(dict1, dict2):
+    with open(output_file, 'w+') as f:
+        f.write("<ROUGE_EVAL version=\"%s\">\n" % (version))
 
-for f in files:
-    tokens = f.split('.')
+        def recurse_vividict(dict1, dict2, depth, path):
+            for key, val in sorted(dict1.items()):
+                new_path = list(path)
+                new_path.append(key)
 
-    token1 = tokens[0]
-    token2 = tokens[1]
-    token3 = tokens[2]
-    token4 = tokens[3]
-    token5 = tokens[4]
+                if depth == 4:
+                    partial_path = ".".join(new_path)
 
-    d[token1][token2][token3][token4][token5]
+                    f.write("<EVAL ID=\"%s\">\n" % (partial_path))
+                    f.write("<PEER-ROOT>\n")
+                    f.write(system_root + "\n")
+                    f.write("</PEER-ROOT>\n")
+                    f.write("<MODEL-ROOT>\n")
+                    f.write(model_root + "\n")
+                    f.write("</MODEL-ROOT>\n")
 
-model_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-model_dict = Vividict()
+                    f.write("<INPUT-FORMAT TYPE=\"SPL\">\n")
+                    f.write("</INPUT-FORMAT>\n")
+                    f.write("<PEERS>\n")
 
-for g in model_files:
-    tokens = g.split('.')
+                    for x, y in val.items():
+                        f.write("<P ID=\"%s\">%s</P>\n" % (x, ".".join([partial_path, x])))
 
-    token1 = tokens[0]
-    token2 = tokens[1]
-    token3 = tokens[2]
-    token4 = tokens[3]
-    token5 = tokens[4]
+                    f.write("</PEERS>\n")
+                    f.write("<MODELS>\n")
 
-    model_dict[token1][token2][token3][token4][token5]
+                    for x, y in sorted(dict2[path[0]][path[1]][path[2]].items()):
+                        f.write("<M ID=\"%s\">%s</M>\n" % (x, ".".join([partial_path, x])))
 
-print("<ROUGE_EVAL version=\"%s\">" % (version))
-this_list = recursive_print(d, model_dict, 0, "")
-print("</ROUGE_EVAL>")
+                    f.write("</MODELS>\n")
+                    f.write("</EVAL>\n")
+
+                else:
+                    recurse_vividict(val, dict2, depth+1, new_path)
+
+        recurse_vividict(dict1, dict2, 1, list())
+        f.write("</ROUGE_EVAL>\n")
+
+system_files = [f for f in listdir(system_root) if isfile(join(system_root, f))]
+system_dict = create_vividict(system_files)
+
+model_files = [f for f in listdir(model_root) if isfile(join(model_root, f))]
+model_dict = create_vividict(model_files)
+
+create_settings_xml(system_dict, model_dict)
+
+#pp = pprint.PrettyPrinter()
+#pp.pprint(model_dict)
+#pp.pprint(system_dict)
