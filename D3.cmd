3120651fbbe6fc5b42fddfe5ad626ef6cbfd129b
Travis Nguyen <travin@uw.edu>
2017-05-14 17:04:38 -0700

    fixed bug with D3.cmd

--- D3.cmd
+++ D3.cmd
@@ -1,10 +1,10 @@
-executable = centroid.py
+executable = /bin/bash/python3
 universe = vanilla
 getenv = true
-output = D2.out
-error = D2.error
-log = D2.log
-arguments = corpora.json --size 100 --topN 10 --corpus reuters --centWeight 5 --posWeight 1 --first 1 --red 100 --topW 1
+output = D3.out
+error = D3.error
+log = D3.log
+arguments = "centroid.py corpora.json --size 100 --topN 10 --corpus reuters --centWeight 5 --posWeight 1 --first 1 --red 100 --topW 1"
 transfer_executable = false
 request_memory = 1024*2
 queue
