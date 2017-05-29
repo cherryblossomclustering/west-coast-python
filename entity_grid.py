8bdacfa6ab10a4bfbc4b7aa3bd86dfa56bc91234
Travis Nguyen <travin@uw.edu>
2017-05-28 21:52:14 -0700

    old D4

---
+++ entity_grid.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+import nltk
+
+class EntityGrid(object):
+
+    def __init__(self, sentences):
+        # should be in a list data structure
+        self.sentences = sentences
+
+        dfdf
