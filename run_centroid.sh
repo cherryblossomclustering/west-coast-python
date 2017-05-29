ca1a984bb44b06a16d8329f153f41d5c812725e3
Karen <KarenLKincy@gmail.com>
2017-05-28 19:11:00 -0700

    Removed hard-coded paths for cached out files

--- run_centroid.sh
+++ run_centroid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 centroid.py compressed-eval-corpora.json --size 600 --topN 60 --corpus wikipedia --wikiScores /home2/kincyk/573/wikipedia/wikipediaScores50000.json --wikiWeight 200 --wikiIDF /home2/kincyk/573/wikipedia/wikipediaIDF50000.json --wikiCBOW /home2/kincyk/573/wikipedia/wikipediaCBOW50000mincount2
+python3 centroid.py compressed-eval-corpora.json --size 600 --topN 60 --corpus wikipedia --wikiScores wikipediaScores50000.json --wikiWeight 200 --wikiIDF wikipediaIDF50000.json --wikiCBOW wikipediaCBOW50000mincount2
